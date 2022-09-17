753a1d77e9870abda7e8013de281171d7fde173c
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-09-17 14:41:59 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use std::io;
-fn main() {
+//use std::io;
 
-
-    // matrix template  
-    
-
-
-    #[derive(Debug)]
-    struct Matrix {
+#[derive(Debug, Clone)]
+    pub struct Matrix {
     vector: Vec<i32>,
     rows: u32,
     colloms: u32,
@@ -15,8 +9,23 @@ fn main() {
     }
 
 
-   //  controls
+
+fn main() {
+
+
+
+    // optional IO input
    
+/*
+    println!("operator?");
+
+    let mut action_1 = String::new();
+    io::stdin().read_line(&mut action_1).ok();
+
+    let action = action_1.trim().chars().next().expect("string is empty");
+
+
+    */
 
     let action = '*';
 
@@ -26,53 +35,32 @@ fn main() {
 
     let m1 = Matrix {
 
-        vector: [0, 1, 2, 3].to_vec(),
-        rows: 2,
-        colloms: 2,
+        vector: vec![0, 1, 2, 3, 4, 5, 6, 7, 8],
+        rows: 3,
+        colloms: 3,
 
     };
 
 
     let m2 = Matrix {
 
-        vector: [0, 1, 2, 3].to_vec(),
-        rows: 2,
-        colloms: 2,
+        vector: vec![0, 1, 2, 3, 4, 5, 6, 7, 8],
+        rows: 3,
+        colloms: 3,
 
     };
 
-    let mut m3 = Matrix {
-
-        vector: [0].to_vec(),
-        rows: 0,
-        colloms: 0,
-    };
 
     let mut m4 = Matrix {
-        vector: [0].to_vec(),
+
+        vector: vec![],
         rows: 0,
         colloms: 0,
+
     };
 
+   
     
-    
-
-
-    // var init
-
-    let mut pm1 = 0;
-
-    let mut add = false;
-
-    let mut min = false;
-
-    let mut addmin = false;
-
-    let mut multi = false;
-
-    let mut vecm = 0;
-
-
 
     // error check
 
@@ -83,170 +71,158 @@ fn main() {
             if m1.vector.len() == m2.vector.len() {
 
                 if action == '+' {
-                    add = true;
-                    addmin = true
-
+                    println!("adding");
+                    m4 = add_m(m1.clone(), m2.clone());
 
                 }else if action == '-' {
-                    min = true;
-                    addmin = true;
-            }
 
-            if m1.colloms == m2.rows {
-                if action == '*' {
+                    println!("subtracting");
+                    m4 = min_m(m1.clone(), m2.clone());
 
-                multi = true;
+                }else if m1.colloms == m2.rows {
+                    if action == '*' {
+
+                    println!("multiplying");
+                    m4 = multi_m(m1.clone(), m2.clone());
+
+
+        
+                    }else {
+                    println!("invalid operator");
+                    return
+
+                    }
                 }
             }
         }
     }
 
+    println!("{}", pr_m(m4));
+
+
+}
+
+pub fn min_m(m1: Matrix, m2: Matrix) -> Matrix {
+
+    let rows = m1.rows;
+    let colloms = m1.colloms;
+
+    let mut m3 = Matrix {
+        vector: vec![],
+        rows: rows,
+        colloms: colloms
+    };
+
+    for x in 0..m1.vector.len() {
+
+        m3.vector.push( m1.vector[x] - m2.vector[x])
+    };
+
+
+    return m3
+
+}
+
+
+pub fn add_m(m1: Matrix, m2: Matrix) -> Matrix {
+
+    let rows = m1.rows;
+    let colloms = m1.colloms;
+    
+    let mut m3 = Matrix {
+        vector: vec![],
+        rows: rows,
+        colloms: colloms
+    };
+    
+    for x in 0..m1.vector.len() {
+
+        m3.vector.push( m1.vector[x] + m2.vector[x]);
+        };
+
+    return m3
+}
+
+
+pub fn multi_m(m1: Matrix, m2: Matrix) -> Matrix {
+
+
+    // rows & colloms
+
+    let rows = m1.rows;
+    let colloms = m2.colloms;
+    
+    let mut m4 = Matrix {
+        vector: vec![],
+        rows: rows,
+        colloms: colloms,
+    };
+
+    for _x in 0..rows * colloms {
+
+
+        m4.vector.push(0)
+
     }
 
 
+    println!("{rows} x {colloms}");
 
+    let mut x = 0;
+    let mut y = 0;
+    let mut z = 0;
     
+    for _x in 0..m4.rows {
+        for _y in 0..m4.colloms {
+            for _z in 0..m1.colloms {
 
-    println!("\n{}\n{}\n{}", add, min,  multi);
-  
+                m4.vector[z] += m1.vector[x] * m2.vector[y];
 
-    // addition
-
-
-    if addmin == true  {
-
-        m3.rows = m1.rows;
-        m3.colloms = m1.colloms;
-
-        for x in 0..m1.vector.len() -1  {
-
-            m3.vector.push(0);
- 
-        }; 
-
-
-
-        if add == true {
-        for x in 0..m1.vector.len() {
-
-
-            m3.vector[x] = m1.vector[x] +  m2.vector[x];
-        }
-
-        }else { 
-            for x in 0..m1.vector.len() {
-
-                m3.vector[x] = m1.vector[x] - m2.vector[x];
+                x += 1;
+                y += rows as usize;
             };
 
-        
-        }
+            y -= (rows * m1.colloms) as usize -1;
+            z += 1;
+            x -= m1.colloms as usize;
+       
+        };
 
+        y -= rows as usize;
+        x += m1.colloms as usize;
+    };
 
-    }else if action == '*' {
-
-
-        if multi == true {
-
-
-
-        m3.rows = m1.colloms;
-        m3.colloms = m2.rows;
-
-        m4.rows = m3.rows;
-        m4.colloms = m3.colloms;
-
-        for x in 0..m3.colloms * m3.rows -1 {
-
-            m3.vector.push(0);
-            m4.vector.push(0);
-
-        }
-
-//        println!("{:#?}", m3);
-
-
-        // multiplication var init
-
-       let mut a = 0;
-       let mut b = 0;
-       let mut c = 0;
-       let mut d = 0;
-       let mut e = 0;
-       let rowin = m3.rows as usize;
-       let colin = m3.colloms as usize;
-
-
-
-       for z in 0..m3.rows {
-
-        for y in 0..m3.colloms {
-
-
-            for x in 0..m3.colloms {
-
-
-        m4.vector[e] = m1.vector[b] * m2.vector[c];
-
-
-
-        m3.vector[d] = m3.vector[d] + m4.vector[e];
-
-        a = a + 1;
-        b = b + 1;
-        c = c + rowin;
-        e = e + 1;
-        
-            }
-
-            e = 0;
-            
-            d = d + 1;
-
-            a = 0;
-
-           b = b - colin; 
-            c = c - (rowin * colin - 1);
-
-
-            
-        }
-
-        e = 0;
-
-
-        b = b + colin;
-        c = c - rowin;
-
-       }
-
-        }
+       return m4
+}
 
         
 
-    }else {
-        println!("invalid operation");
 
-    }
+pub fn pr_m(m: Matrix) -> String {
 
-    // print section
+    if m.vector.len() == 0 {
+        return "invalid matrix".to_string()
+    };
+    let mut stdout = String::new();
+    let mut pm1 = 0;
 
-   
+     for _x in 0..m.rows {
 
-    for x in 0..m3.rows {
+        stdout += &format!("( ").to_string();
 
-        print!("( ");
+        for _y in 0..m.colloms {
+            stdout += &format!("{} ", m.vector[pm1]).to_string();
 
-        for y in 0..m3.colloms {
-            print!("{} ", m3.vector[pm1]);
-
-        pm1 = pm1 + 1;
+        pm1 += 1;
 
         };
 
 
-        print!(")\n");
+        stdout += &format!(")\n").to_string();
     };
+
+
+     return stdout
 
 
 
